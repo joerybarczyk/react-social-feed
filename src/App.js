65896b429d5a3984f1966b7398a8e8661b26e508
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-04-20 11:59:30 -0400

    Create NavBar Component + Create and add NavBar styles from NavBar.css + Add NavBar component to App component + Change document title to "SocialFeed"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import NavBar from './components/NavBar/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css'
+
+function NavBar(props) {
+    return ( 
+        <nav className="nav-body">
+            <h1 className="logo">Social<em>Feed</em></h1>
+        </nav>
+     );
+}
+
+export default NavBar;
